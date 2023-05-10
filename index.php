8684c0f89ecd6a7886cc01bf3136c72ab515a9c2
Jenil Thakor <jenil.rohit45@gmail.com>
2023-05-10 16:27:34 +0530

    front end setup

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-include './database/_dbconnect.php'
+include './database/_dbconnect.php';
+include './navbar.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    Hello Starter
-</body>
-
-</html>
--- navbar.php
+++ navbar.php
@@ -6,17 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title><?php echo $page_title; ?></title>
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <!-- Font Awesome Icon -->
-    <link rel="stylesheet" href="css/font-awesome.css">
-    <!-- Custom stlylesheet -->
-    <link rel="stylesheet" href="css/style.css">
+    <title>Hi</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 
 <body>
     <!-- HEADER -->
-    <div id="navbar">
+    <div id="text-[red]" class="text-[red]">
         Navbar
     </div>
